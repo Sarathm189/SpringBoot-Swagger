536c0deeae4d33409fce6ac488f54c11cce0faec
Sarath Kumar M <msarath1@pkrmsezdc.com>
2021-08-23 19:24:49 +0530

    step3-enabling Swagger2

---
+++ SpringBootSwagger2Application/src/main/java/com/example/SpringBootSwagger2Application/configuration/SwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.example.SpringBootSwagger2Application.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+}
--- SpringBootSwagger2Application/src/main/java/com/example/SpringBootSwagger2Application/controllers/AddressBookResource.java
+++ SpringBootSwagger2Application/src/main/java/com/example/SpringBootSwagger2Application/controllers/AddressBookResource.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.SpringBootSwagger2Application.models.Contact;
 
 @RestController
+@RequestMapping("/api")
 public class AddressBookResource {
 	ConcurrentHashMap<String, Contact> contacts = new ConcurrentHashMap();
 

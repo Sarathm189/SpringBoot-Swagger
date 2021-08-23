27c1ffcf9170022ada3b0f43e2c56d5e1117a13e
Sarath Kumar M <msarath1@pkrmsezdc.com>
2021-08-23 18:43:18 +0530

    step2-api creation

---
+++ SpringBootSwagger2Application/src/main/java/com/example/SpringBootSwagger2Application/models/Contact.java
@@ -0,0 +1,29 @@
+package com.example.SpringBootSwagger2Application.models;
+
+public class Contact {
+	
+	private String id;
+	private String name;
+	private String phoneNo;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+	
+	
+
+}
